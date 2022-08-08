6bd8243fea15fc4165a7f5906992e16924b2727a
Jorge Sánchez Medina <jorge.sanchezmedina@plexus.es>
2022-08-08 10:08:38 +0100

    fix naming

--- app/src/main/java/com/jorgeSM/apimarvel/Const.kt
+++
@@ -1,7 +0,0 @@
-package com.jorgeSM.apimarvel
-
-object Const {
-    const val API_END_POINT = "https://gateway.marvel.com"
-    const val CHARACTER_LIST_PATH = "/v1/public/characters"
-    const val API_PUBLIC_KEY = "fc9b4aa59e2c004ecb5ad993ef366531"
-}
---
+++ app/src/main/java/com/jorgeSM/apimarvel/utils/Const.kt
@@ -0,0 +1,9 @@
+package com.jorgeSM.apimarvel.utils
+
+object Const {
+    const val BASE_URL = "https://gateway.marvel.com/"
+    const val CHARACTER_LIST_PATH = "v1/public/characters?"
+    const val API_PUBLIC_KEY = "fc9b4aa59e2c004ecb5ad993ef366531"
+    const val TIME_STAMP = 1
+
+}
