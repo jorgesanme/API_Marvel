fd16821befc68737410ec4a18fbe0a336a5e1078
Jorge Sánchez Medina <jorge.sanchezmedina@plexus.es>
2022-08-12 13:45:35 +0100

    separate models class

---
+++ app/src/main/java/com/jorgeSM/apimarvel/remote/models/ComicsItem.kt
@@ -0,0 +1,6 @@
+package com.jorgeSM.apimarvel.remote.models
+
+data class ComicsItem(
+    val resourceURI: String? = null,
+    val name: String? = null
+)
---
+++ app/src/main/java/com/jorgeSM/apimarvel/remote/models/Data.kt
@@ -0,0 +1,9 @@
+package com.jorgeSM.apimarvel.remote.models
+
+data class Data(
+    val offset: Long? = null,
+    val limit: Long? = null,
+    val total: Long? = null,
+    val count: Long? = null,
+    val results: List<Result>? = null
+)
