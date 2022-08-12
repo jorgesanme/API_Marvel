1d65d81842787e2db100559e45dcf0715881a914
Jorge Sánchez Medina <jorge.sanchezmedina@plexus.es>
2022-08-12 13:54:35 +0100

    extract createEndPointHash into Utils

--- app/src/main/java/com/jorgeSM/apimarvel/utils/Utils.kt
+++ app/src/main/java/com/jorgeSM/apimarvel/utils/Utils.kt
@@ -5,8 +5,17 @@ import java.security.MessageDigest
 
 object Utils {
 
-    fun createMD5(input: String): String{
+    private fun createMD5(input: String): String {
         val messageDigest = MessageDigest.getInstance("MD5")
-        return BigInteger(1,messageDigest.digest(input.toByteArray())).toString(16).padStart(32,'0')
+        return BigInteger(1, messageDigest.digest(input.toByteArray())).toString(16)
+            .padStart(32, '0')
+    }
+
+    fun createEndPointHash(): String {
+        return createMD5(
+            Const.TIME_STAMP.toString()
+                    + Const.API_PRIVATE_KEY
+                    + Const.API_PUBLIC_KEY
+        )
     }
 }
